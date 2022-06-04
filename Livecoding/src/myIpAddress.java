c005b2fb14d1c5a2c357c4d4e0c8b1667c3dbcc6
USER <USER@USER-PC>
2022-06-04 15:37:16 +0530

    HI,FIRST REPO

---
+++ Livecoding/src/myIpAddress.java
@@ -0,0 +1,16 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class myIpAddress {
+
+	public static void main(String[] args) {
+		try {
+			InetAddress ip=InetAddress.getLocalHost();
+			System.out.println("my IP addrs:"+ip.getHostAddress());
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
